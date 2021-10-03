70534d4f6ffc19c9968ffe4db99fe5e0468821bd
Jordan Harband <ljharb@gmail.com>
2021-10-02 21:22:53 -0700

    [Refactor] remove ESM entry points

--- test/index.mjs
+++ test/index.mjs
@@ -16,9 +16,9 @@ test('named exports', async (t) => {
 	);
 
 	const { shim, getPolyfill, implementation } = truncModule;
-	t.equal(await import('math.trunc/shim'), shim, 'shim named export matches deep export');
-	t.equal(await import('math.trunc/implementation'), implementation, 'implementation named export matches deep export');
-	t.equal(await import('math.trunc/polyfill'), getPolyfill, 'getPolyfill named export matches deep export');
+	t.equal((await import('math.trunc/shim')).default, shim, 'shim named export matches deep export');
+	t.equal((await import('math.trunc/implementation')).default, implementation, 'implementation named export matches deep export');
+	t.equal((await import('math.trunc/polyfill')).default, getPolyfill, 'getPolyfill named export matches deep export');
 
 	t.end();
 });
